9cb8e892ba16fe377e8d190a4decd863593ce6c8
Donghyuk Lee <leedonghyuk2163@gmail.com>
2024-09-02 11:30:38 +0900

    init Commit

---
+++ src/main/java/com/tvod_test/test_dev/rest/entity/UserEntity.java
@@ -0,0 +1,29 @@
+package com.tvod_test.test_dev.rest.entity;
+
+import com.tvod_test.test_dev.rest.entity.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Table(name = "tb_user")
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "user_name", nullable = false)
+    private String userName;
+
+    private String useYn;
+}
---
+++ src/main/java/com/tvod_test/test_dev/rest/entity/base/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.tvod_test.test_dev.rest.entity.base;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createDate;
+    private String createId;
+    @LastModifiedDate
+    private LocalDateTime updateDate;
+    private String updateId;
+}
